27c52e96889386265950cf0746ac5c1b0d766098
Ben Smith <benlsmith329@gmail.com>
2022-07-16 14:38:09 -0400

    push

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,35 +3,39 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
+    // Entry point for files
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "Webpack Plugin"
+        template: './index.html',
+        title: 'PWA text editor'
       }),
+     
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'PWA Text Editor',
-        short_name: 'Text Editor',
-        description: 'Basic text editor',
+        name: 'PWA text editor',
+        short_name: 'text editor',
+        description: 'A program that allows you to create and edit text files!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
@@ -45,23 +49,26 @@ module.exports = () => {
         ],
       }),
     ],
+
     module: {
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"]
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            }
-          }
-        }
+            },
+          },
+        },
       ],
     },
   };
